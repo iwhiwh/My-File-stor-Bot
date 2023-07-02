a477af7ee25384500e7b288da5b58b2b0ebbdec4
DextiNxD <95567528+DextiNxD@users.noreply.github.com>
2023-07-02 19:05:16 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -11,4 +11,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("DigitalRelease")
+    return web.json_response("CodeXBotz")
