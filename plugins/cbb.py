9fae207a1de85ea2c3f589de6ce89647fec1d985
iwhiwh <131843374+iwhiwh@users.noreply.github.com>
2023-07-28 12:02:17 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Main Channel : <a href='https://t.me/The_Happy_Hour_Hindi'>The Happy Hour</a>\n○ Channel : @The_Happy_Hour_Hindi\n○ Support Group : @Happy_Hour_Friends</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>Ash Ketchum</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Main Channel : <a href='https://t.me/The_Happy_Hour_Hindi'>The Happy Hour</a>\n○ Channel : @The_Happy_Hour_Hindi\n○ Support Group : @Happy_Hour_Friends</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
