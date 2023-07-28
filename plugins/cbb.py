27b3ffc7baf9683d7eb9cda536732c5216c7146d
iwhiwh <131843374+iwhiwh@users.noreply.github.com>
2023-07-28 12:29:12 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ 😎Creator😎 : <a href='tg://user?id={OWNER_ID}'>Ash Ketchum</a>\n○ 🥲Language🥲 : <code>Hindi</code>\n○ 😉Library😉 : <a href='https://docs.pyrogram.org/'>Indian library🇮🇳</a>\n○ 🥰Main Channel🥰 : <a href='https://t.me/The_Happy_Hour_Hindi'>The Happy Hour</a>\n○ 🥳Support Group🥳 : <a href='https://t.me/Happy_Hour_Hindi'>Click Here</a></b>",
+            text = f"<b>○ 😎Creator😎 : <a href='https://t.me/Mister_Ash'>Ash Ketchum</a>\n○ 🥲Language🥲 : <code>Hindi</code>\n○ 😉Library😉 : <a href='https://docs.pyrogram.org/'>Indian library🇮🇳</a>\n○ 🥰Main Channel🥰 : <a href='https://t.me/The_Happy_Hour_Hindi'>The Happy Hour</a>\n○ 🥳Support Group🥳 : <a href='https://t.me/Happy_Hour_Hindi'>Click Here</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
